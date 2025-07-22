a088a105c3d8c9fd074cbbfa1c9646d9fd343c04
ojas-chaturvedi <ojaschaturvedi06@gmail.com>
2025-07-22 00:57:22 -0700

    Update final notification to ping me

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -9,6 +9,9 @@
 # Send final notification
 
 curl -s -X POST -H "Content-Type: application/json" -d "{
-\"content\": \"**All jobs have finished running**\",
-\"avatar_url\": \"https://droplr.com/wp-content/uploads/2020/10/Screenshot-on-2020-10-21-at-10_29_26.png\"
+\"content\": \"<@746026689397653534> **All jobs have finished running**\",
+\"avatar_url\": \"https://droplr.com/wp-content/uploads/2020/10/Screenshot-on-2020-10-21-at-10_29_26.png\",
+\"allowed_mentions\": {
+    \"users\": [\"746026689397653534\"]
+}
 }" https://discord.com/api/webhooks/1355780352530055208/84HI6JSNN3cPHbux6fC2qXanozCSrza7-0nAGJgsC_dC2dWAqdnMR7d4wsmwQ4Ai4Iux >/dev/null
