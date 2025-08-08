b06c7b14db8215c4595fb038201564095f6a8a4f
ojas-chaturvedi <ojaschaturvedi06@gmail.com>
2025-08-07 21:29:45 -0700

    Ignore simple warnings and set a higher recursion limit for deep MIDI files

--- scripts/scoring.py
+++ scripts/scoring.py
@@ -12,6 +12,15 @@ import pretty_midi
 import numpy as np
 import argparse
 import mir_eval
+import sys
+import warnings
+
+# Ignore simple warnings
+warnings.filterwarnings("ignore", message=".*pkg_resources is deprecated.*")
+warnings.filterwarnings("ignore", message=".*not a valid type 0 or type 1 MIDI file.*")
+
+# Set a higher recursion limit for deep MIDI files
+sys.setrecursionlimit(10000)
 
 
 def extract_intervals_and_pitches(midi_file):
