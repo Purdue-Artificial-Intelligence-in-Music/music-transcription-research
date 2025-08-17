8740b5a062e1fd940ac7ba6d2e270bb79a442123
ojas-chaturvedi <ojaschaturvedi06@gmail.com>
2025-08-17 03:47:09 -0700

    Ping user in upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -97,6 +97,9 @@ overall_runtime_formatted=$(printf '%02d:%02d:%02d' "$hours" "$minutes" "$second
 echo "Total runtime: $overall_runtime_formatted"
 
 curl -s -X POST -H "Content-Type: application/json" -d "{
-\"content\": \"Finished uploading results for **$model_name / $dataset_name**\\n.wav files: $num_wavs\\nAvg F-measure: $avg_fmeasure\\nTotal runtime: $overall_runtime_formatted\",
-\"avatar_url\": \"https://droplr.com/wp-content/uploads/2020/10/Screenshot-on-2020-10-21-at-10_29_26.png\"
+\"content\": \"<@746026689397653534> Finished uploading results for **$model_name / $dataset_name**\\n.wav files: $num_wavs\\nAvg F-measure: $avg_fmeasure\\nTotal runtime: $overall_runtime_formatted\",
+\"avatar_url\": \"https://droplr.com/wp-content/uploads/2020/10/Screenshot-on-2020-10-21-at-10_29_26.png\",
+\"allowed_mentions\": {
+    \"users\": [\"746026689397653534\"]
+}
 }" https://discord.com/api/webhooks/1355780352530055208/84HI6JSNN3cPHbux6fC2qXanozCSrza7-0nAGJgsC_dC2dWAqdnMR7d4wsmwQ4Ai4Iux >/dev/null
