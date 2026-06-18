9cbee01423a6be8a9197839a5f1cce129bcc0504
ojas-chaturvedi <ojaschaturvedi06@gmail.com>
2026-06-17 20:27:27 -0700

    Fix traditional flute dataset download and extraction

--- datasets/traditional_flute.sh
+++ datasets/traditional_flute.sh
@@ -17,15 +17,15 @@ ZIP_FILE="traditional_flute.zip"
 # Download and unzip the Traditional Flute dataset, keeping only the MIDI files
 wget -q -O "$ZIP_FILE" "$KAGGLE_URL"
 unzip -q "$ZIP_FILE" -d traditional_flute
-find traditional_flute/traditional-flute-dataset/score -type f -name "*.midi" -exec mv {} traditional_flute/ \;
-rm -rf traditional_flute/traditional-flute-dataset
-rm -f traditional_flute/traditional-flute-dataset.zip
+# find traditional_flute/traditional-flute-dataset/score -type f -name "*.midi" -exec mv {} traditional_flute/ \;
+# rm -rf traditional_flute/traditional-flute-dataset
+rm -f traditional_flute/traditional-flute-dataset.zip traditional_flute.zip
 
 # Rename all .midi files to .mid
-find traditional_flute -type f -name "*.midi" | while read -r file; do
-    mv "$file" "${file%.midi}.mid"
-done
+# find traditional_flute -type f -name "*.midi" | while read -r file; do
+#     mv "$file" "${file%.midi}.mid"
+# done
 
 # Print the number of MIDI files found
-MIDI_COUNT=$(find traditional_flute -type f -name "*.mid" | wc -l)
+MIDI_COUNT=$(find traditional_flute -type f -name "*.midi" | wc -l)
 echo "Found $MIDI_COUNT MIDI files."
